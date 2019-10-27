50c32132ff4c176be98113b501634af087dd6b2d
ndanhkhoi <ngducanhkhoiii@gmail.com>
2019-10-27 17:49:07 +0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import singleSpaReact from 'single-spa-react';
 import * as serviceWorker from './serviceWorker';
+import { appName } from './micro-app/utils';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById(appName));
+
+const reactLifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
+    domElementGetter: () => document.getElementById(appName) as Element
+});
+
+export const bootstrap = [
+    reactLifecycles.bootstrap
+];
+
+export const mount = [
+    reactLifecycles.mount
+];
+
+export const unmount = [
+    reactLifecycles.unmount
+];
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/micro-app/utils.ts
@@ -0,0 +1,26 @@
+export const appName = 'app1';
+
+export const mainApp = 'MAIN_APP';
+const _key = (appName: string, event: string) => `${appName}/${event}`;
+
+export const dispatchMicroAppEvent = async (event: any, agrs: any) => {
+    await sessionStorage.setItem(_key(appName, event), JSON.stringify(agrs));
+    await window.dispatchEvent(new Event(_key(appName, event)));
+};
+
+export const addMicroAppsEventListener = (appName: string, event: string, handleEvent: any) => {
+    window.addEventListener(_key(appName, event), handleEvent);
+};
+  
+export const removeMicroAppsEventListener = (appName: string, event: string, handleEvent: any) => {
+    window.removeEventListener(_key(appName, event), handleEvent);
+};
+
+export const getAgrsOfMicroAppsEvent: any = (appName: string, event: string) => {
+    const str = sessionStorage.getItem(_key(appName, event));
+    if (str !== null) {
+        const tmp = JSON.parse(str);
+        sessionStorage.removeItem(_key(appName, event));
+        return tmp;    
+    } else return null;
+};
