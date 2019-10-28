d64692b55e1e2333f97f556a6d7a3256c571699e
ndanhkhoi <ngducanhkhoiii@gmail.com>
2019-10-28 08:45:48 +0700

    update webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -278,13 +278,13 @@ module.exports = function(webpackEnv) {
         chunks: 'all',
         name: false,
       },
-      */
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
       // https://github.com/facebook/create-react-app/issues/5358
       runtimeChunk: {
         name: entrypoint => `runtime-${entrypoint.name}`,
       },
+      */
     },
     resolve: {
       // This allows you to set a fallback for where Webpack should look for modules.
